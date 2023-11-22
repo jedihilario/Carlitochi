70be7a32cb3daa60d393a7b21947ecac7e3d52eb
jedihilario <hyanezbertola@gmail.com>
2023-11-21 22:57:00 -0300

    Models updated: Pet added

--- pet/admin.py
+++ pet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from pet.models import Pet
 
 # Register your models here.
+
+admin.site.register(Pet)
--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Pet(models.Model):
+    nombre = models.CharField(max_length = 64, null = False)
+    salud = models.IntegerField(null = False, default = 100)
+    hambre = models.IntegerField(null = False, default = 0)
+    diversion = models.IntegerField(null = False, default = 100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__ (self):
+        return self.nombre   
